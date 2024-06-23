6ad9f3312cb7eec460c78092bafd2a57b13ae65e
shiinazzz <shiinazzz.burga@gmail.com>
2024-06-23 14:22:59 +0700

    next.js

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div id="zzz">
+                <img src="/eepy.png"/>
+                <h1>zzz~</h1>
+                <p>
+                    so lonely...<br/><br/><br/><br/>...where am i?
+                </p>
+                <h6>(i'm lazy to make a page)</h6>
+            </div>
+        </main>
+    );
+}
